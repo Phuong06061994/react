fc58968774765ca125ee04a1d5be24e19fa050c8
Nguyễn Văn Phượng <Phuongnv35@fpt.com.vn>
2020-09-16 15:51:20 +0700

    update

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import  React from 'react';  
-const Contact=React.lazy(()=>import('./components/Contact'));  
-
-const routes = [  
-    { path: '/Contact', component: Contact },  
-    
-];  
-  
-export default routes;  
